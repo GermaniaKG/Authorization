f307c9481389d241412c7141cbf4ada4d01e82f5
Carsten Witt <carsten.witt@gmail.com>
2016-12-19 13:32:47 +0100

    Fix autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,3 +12,4 @@ $autoloader_file = __DIR__ . '/../vendor/autoload.php';
 if (!is_readable( $autoloader_file )) {
     die( PHP_EOL . "Missing Composer's vendor/autoload.php; run 'composer update' first." . PHP_EOL . PHP_EOL );
 }
+require_once( $autoloader_file );
