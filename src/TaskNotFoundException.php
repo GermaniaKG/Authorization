c0013b90d0b90f63594f67c8258042ab39c4052b
Carsten Witt <carsten.witt@gmail.com>
2017-07-06 09:36:03 +0200

    Add NotFoundExceptionInterface

--- src/TaskNotFoundException.php
+++ src/TaskNotFoundException.php
@@ -4,7 +4,7 @@ namespace Germania\Authorization;
 use Interop\Container\Exception\NotFoundException;
 use Psr\Container\NotFoundExceptionInterface;
 
-class TaskNotFoundException extends \Exception implements NotFoundException, NotFoundExceptionInterface
+class TaskNotFoundException extends \Exception implements NotFoundExceptionInterface, NotFoundException
 {
 
 }
