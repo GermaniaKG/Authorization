db92c2f7ff3387f69bb6d86ce212507169fd86a5
Carsten Witt <carsten.witt@gmail.com>
2017-09-06 09:30:47 +0200

    Change error message, use require_once

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,6 +10,6 @@
 
 $autoloader_file = __DIR__ . '/../vendor/autoload.php';
 if (!is_readable( $autoloader_file )) {
-    die( PHP_EOL . "Missing Composer's vendor/autoload.php; run 'composer update' first." . PHP_EOL . PHP_EOL );
+    die( PHP_EOL . "Missing Composer's vendor/autoload.php; run 'composer install' first." . PHP_EOL . PHP_EOL );
 }
-require_once( $autoloader_file );
+require_once $autoloader_file;
